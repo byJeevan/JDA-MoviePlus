6f2ad64cca244baed5640294597cf58a9384a77b
jeevan-1382 <a_jeevan.rao@africa.airtel.com>
2020-04-18 13:56:06 +0530

    ui-login

--- SwiftUIMovie/Login/LoginView.swift
+++ SwiftUIMovie/Login/LoginView.swift
@@ -17,6 +17,7 @@ struct LoginView: View {
     var body: some View {
         NavigationView {
             VStack {
+                LoginImage()
                 LoginTitle()
                 UserNameField(userNameString: $loginViewModel.userName)
                 PasswordField(passwordString: $loginViewModel.userPass)
@@ -33,12 +34,26 @@ struct LoginView_Previews: PreviewProvider {
 }
 
 //MARK:- UI Elements
+
+struct LoginImage : View {
+    var body:some View {
+        Image("pop-corn")
+        .resizable()
+        .frame(width: 128, height: 128)
+        .clipShape(Circle())
+        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+         .padding(.bottom, Constants.Styles.bottomPadding20)
+    }
+}
+
 struct LoginTitle : View {
     
     var body : some View {
         Text("Hello!! Login to find out latest movie release.")
             .font(.subheadline)
             .fontWeight(.semibold)
+            .multilineTextAlignment(.center)
             .padding(.bottom, Constants.Styles.bottomPadding20)
     }
 }
